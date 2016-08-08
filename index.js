8a676cf2b630e82e30e7537f24c13f5c527a494a
Dan Kirschner <dpkirschner@gmail.com>
2016-08-07 18:50:27 -0700

    fixing package structure and removing heroku default text

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
+var opbeat = require('opbeat').start()
+
 var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use(opbeat.middleware.express())
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index')
+  response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
